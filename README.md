a32502b0e73698ade6d52f1e4d4329a606fdb0db
Cole Willsea <coleww@gmail.com>
2016-10-12 21:01:08 -0700

    rendering major and minor scales

--- README.md
+++ README.md
@@ -4,9 +4,14 @@ guitar-tab-generator
 ```
   [![NPM](https://nodei.co/npm/guitar-tab-generator.png)](https://nodei.co/npm/guitar-tab-generator/)
   [![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat)](https://github.com/feross/standard)
-  [![Build Status](https://secure.travis-ci.org/YR_TRAVIS_USERNAME/guitar-tab-generator.png)](http://travis-ci.org/YR_TRAVIS_USERNAME/guitar-tab-generator)
 ```
 
 ### EXAMPLE
 
 ### API
+
+`function guitarTabGenerator (tonic, scale, guitarTuning(optional))` 
+
+- tonic: should be a capitalized string representation of the tonic note, i.e, "C", "D", "D#". (note: does not recognize flats, only sharps, ONLY!)
+- scale: currently handles 'major', and 'minor'
+- guitarTuning: optional, defaults to ['E', 'A', 'D', 'G', 'B', 'E']. 
--- test.js
+++ test.js
@@ -2,7 +2,50 @@ var tap = require('tap')
 
 var guitarTabGenerator = require('./')
 
-tap.test('does the thing', function (t) {
+tap.test('renders a C major scale', function (t) {
   t.plan(1)
-  t.equal(guitarTabGenerator('world'), 'hello world', 'does it')
+  const cMajorScale = `E|-0--1-----3-----5-----7--8-----10----12-|
+B|-0--1-----3-----5--6-----8-----10----12-|
+G|-0-----2-----4--5-----7-----9--10----12-|
+D|-0-----2--3-----5-----7-----9--10----12-|
+A|-0-----2--3-----5-----7--8-----10----12-|
+E|-0--1-----3-----5-----7--8-----10----12-|`
+
+  t.equal(guitarTabGenerator('C', 'major'), cMajorScale)
+})
+
+tap.test('renders an E major scale', function (t) {
+  t.plan(1)
+  const eMajorScale = `E|-0-----2-----4--5-----7-----9-----11-12-|
+B|-0-----2-----4--5-----7-----9--10----12-|
+G|----1--2-----4-----6-----8--9-----11----|
+D|----1--2-----4-----6--7-----9-----11----|
+A|-0-----2-----4-----6--7-----9-----11-12-|
+E|-0-----2-----4--5-----7-----9-----11-12-|`
+
+  t.equal(guitarTabGenerator('E', 'major'), eMajorScale)
+})
+
+tap.test('renders an A minor scale', function (t) {
+  t.plan(1)
+  const aMinorScale = `E|-0--1-----3-----5-----7--8-----10----12-|
+B|-0--1-----3-----5--6-----8-----10----12-|
+G|-0-----2-----4--5-----7-----9--10----12-|
+D|-0-----2--3-----5-----7-----9--10----12-|
+A|-0-----2--3-----5-----7--8-----10----12-|
+E|-0--1-----3-----5-----7--8-----10----12-|`
+
+  t.equal(guitarTabGenerator('A', 'minor'), aMinorScale)
+})
+
+tap.test('renders an alternate guitar tuning', function (t) {
+  t.plan(1)
+  const cMajorScale = `E|-0--1-----3-----5-----7--8-----10----12-|
+B|-0--1-----3-----5--6-----8-----10----12-|
+G|-0-----2-----4--5-----7-----9--10----12-|
+D|-0-----2--3-----5-----7-----9--10----12-|
+A|-0-----2--3-----5-----7--8-----10----12-|
+D|-0-----2--3-----5-----7-----9--10----12-|`
+
+  t.equal(guitarTabGenerator('C', 'major', ['D', 'A', 'D', 'G', 'B', 'E']), cMajorScale)
 })
